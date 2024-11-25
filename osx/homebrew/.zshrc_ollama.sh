015b5d96138a31de46dcb16ee7e15cdd5af93338
Tom Bury <tom.bury@hotmail.com>
2024-11-25 12:15:07 +0000

    fix: speedup ollama_start

--- osx/homebrew/.zshrc_ollama.sh
+++ osx/homebrew/.zshrc_ollama.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
 function ollama_start() {
+    pkill ollama
     ollama serve > /dev/null 2>&1 &;
-
-    sleep 5
-
-    ollama run llama3.1:8b test
-    ollama run starcoder2:3b test
+    sleep 1
+    ollama run llama3.1:8b ""
+    ollama run starcoder2:3b ""
+    ollama ps
 }
