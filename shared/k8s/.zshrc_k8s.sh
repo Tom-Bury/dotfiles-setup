2dee6609f3a526c186c665733c99e46dda351ca4
Tom Bury <tom.bury@hotmail.com>
2025-02-17 09:46:20 +0000

    feat: add get pods alias

--- shared/k8s/.zshrc_k8s.sh
+++ shared/k8s/.zshrc_k8s.sh
@@ -3,7 +3,7 @@
 k8sc() {
     contexts=$(kubectl config get-contexts --no-headers -o name)
     
-    selection=$(echo $contexts | fzf --prompt='Select k8 context > ' \
+    selection=$(echo $contexts | fzf --prompt='Select K8s context > ' \
                 --color='prompt:#af5fff,header:#262626,gutter:-1,pointer:#af5fff' \
                 --reverse \
                 --pointer="⏺" \
@@ -16,3 +16,24 @@ k8sc() {
 
     echo "kubectl config current-context: $(kubectl config current-context)"
 }
+
+k8sp() {
+    namespaces=$(kubectl -o name get namespaces)
+
+    selection=$(echo $namespaces | fzf --prompt='Select K8s namespace > ' \
+                --color='prompt:#af5fff,header:#262626,gutter:-1,pointer:#af5fff' \
+                --reverse \
+                --pointer="⏺" \
+                --query="$1"
+    )
+
+    namespace="${selection##*/}"
+
+    if [[ -n $namespace ]]; then
+        kubectl get pods --namespace $namespace
+    else
+        echo "No namespace found in selection '$selection'"
+    fi
+}
+
+alias k8s='kubectl'
